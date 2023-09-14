e2925afbcf33930daba4adf0a473f01edeecdaff
Muhammad Assadi <s5223232@student.ad.griffith.edu.au>
2023-09-14 16:21:50 +1000

    Added code to print data table

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,37 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor() 
+
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+result = cursor.fetchall()
+
+print("Year   | CO2    | Temperature")
+print("-----------------------------")
+for r in result:
+    print(f"{r[0]:<7} | {r[1]:<7} | {r[2]:<11}")
+
 years = []
 co2 = []
 temp = []
 
+for r in result:
+    year, co2_value, temp_value = r
+    years.append(year)
+    co2.append(co2_value)
+    temp.append(temp_value)
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+
+plt.show()
+plt.savefig("co2_temp_1.png")
